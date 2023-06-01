af7a63e508b1547706ba9cee618558ae91d8241e
Jaime Leandro Juarez Reyes <126726782+LEAN-REY@users.noreply.github.com>
2023-05-31 23:45:08 -0600

    Update arreglos.js
    se modifo arreglos

--- arreglos.js
+++ arreglos.js
@@ -1,18 +1,6 @@
-function insertElement(arr, element, position) {
-    if (position < 0 || position > arr.length) {
-      console.log("La posición está fuera del rango del arreglo.");
-      return arr;
-    }
-  
-    arr.splice(position, 0, element);
-    return arr;
-  }
-  
-  // Ejemplo de uso
-  var arreglo = [1, 2, 3, 4, 5];
-  var nuevoElemento = 10;
-  var posicion = 2;
-  
-  var arregloModificado = insertElement(arreglo, nuevoElemento, posicion);
-  console.log(arregloModificado);
-  
+let arr = [1,2,3,4,5];
+let index = 3;
+let element = 10;
+arr.splice(index, 0, element);
+
+console.log(arr);
